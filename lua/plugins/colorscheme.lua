a7d44bd7d69abc37cb4c75587e6de61de2d61b63
cipher <coder0807@gmail.com>
2024-11-23 08:29:45 +0530

    added harpoon, toggleterm and bufferline

--- lua/plugins/colorscheme.lua
+++ lua/plugins/colorscheme.lua
@@ -1,11 +1,21 @@
 return {
-  'catppuccin/nvim',
-	name = 'catppuccin',
-	priority = 1000,
-	init = function()
-		vim.cmd.colorscheme 'catppuccin'
+	{
+		"catppuccin/nvim",
+		name = "catppuccin",
+		priority = 1000,
+		init = function()
+			-- require("catppuccin").setup({
+			--		transparent_background = true
+			-- })
 
-		-- You can configure highlights by doing something like:
-		vim.cmd.hi 'Comment gui=none'
-	end,
+			vim.cmd.colorscheme("catppuccin")
+
+			-- You can configure highlights by doing something like:
+			vim.cmd.hi("Comment gui=none")
+		end,
+	},
+	{
+		"rose-pine/neovim",
+		name = "rose-pine",
+	},
 }
